2b08a370753f80a3d43875a3b54c79de8a5866ad
Hernandes <Hernandeshass910@gmail.com>
2024-10-23 15:01:35 -0300

    feat: adicionando criação de nova doação pg doador

--- src/newclothes/menuDoador.java
+++ src/newclothes/menuDoador.java
@@ -3,8 +3,10 @@ package newclothes;
 import conexao.conexao;
 import components.components;
 import javax.swing.*;
+import java.util.Date;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class menuDoador extends JFrame {
@@ -31,6 +33,33 @@ public class menuDoador extends JFrame {
         // Botão Fazer Doação
         JButton novaDoacaoButton = components.criarBotao("Nova Doação","<b>","#8C3A1C","Arial",Font.PLAIN,16,Color.WHITE,20,50,150,30);
         add(novaDoacaoButton);
+
+        // Adiciona ActionListener ao botão
+        novaDoacaoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new fazerDoacao();
+                
+                con_cliente = new conexao();
+                con_cliente.conecta();
+
+                Date data = new Date();
+                java.sql.Date sqlDate = new java.sql.Date(data.getTime()); // Convert to sql.Date
+                int idDoador = 1;
+                int idOng = 1;
+
+                try {
+                    String insert_sql = "insert into doacao (dataDoacao,ID_doador,ID_ong) values ('"+sqlDate+"','"+idDoador+"','"+idOng+"')";
+                    con_cliente.statement.executeUpdate(insert_sql);
+                    JOptionPane.showMessageDialog(null, "Gravação realizada com sucesso!!", "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+
+                } catch (SQLException erro) {
+                    JOptionPane.showMessageDialog(null, "\n Erro na gravação de nova doação: \n" +erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+
+
 
         // Label Doações
         JLabel doacoesLabel = components.criarLabel("FAZER DOAÇÃO", "<u>", "Arial", 14, Font.BOLD, 20, 90, 150, 30);
