c3d6394d06f00f8e9d81f40d7514048cda284551
gustavorods <gustavo.rlsilva07@gmail.com>
2024-12-02 01:00:58 -0300

    ops kkkkk

--- database/bdnewclothes.sql
+++ database/bdnewclothes.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 22/10/2024 às 01:33
--- Versão do servidor: 8.3.0
--- Versão do PHP: 8.2.18
+-- Host: 127.0.0.1
+-- Generation Time: Dec 02, 2024 at 04:53 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,203 +18,145 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `bdnewclothes`
+-- Database: `bdnewclothes`
+    CREATE DATABASE `bdnewclothes`;
+    USE `bdnewclothes`;
 --
-CREATE DATABASE `bdnewclothes`;
-USE `bdnewclothes`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categoria`
+-- Table structure for table `administrador`
 --
 
-DROP TABLE IF EXISTS `categoria`;
-CREATE TABLE IF NOT EXISTS `categoria` (
-  `cod` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(80) NOT NULL,
-  `descricao` varchar(300) NOT NULL,
-  PRIMARY KEY (`cod`)
+CREATE TABLE `administrador` (
+                                 `email` varchar(100) NOT NULL,
+                                 `senha` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `administrador`
+--
+
+INSERT INTO `administrador` (`email`, `senha`) VALUES
+    ('adm@adm', '123');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `doacao`
+-- Table structure for table `categoria`
 --
 
-DROP TABLE IF EXISTS `doacao`;
-CREATE TABLE IF NOT EXISTS `doacao` (
-  `ID_doacao` int NOT NULL AUTO_INCREMENT,
-  `dataDoacao` date DEFAULT NULL,
-  `ID_doador` int NOT NULL,
-  `ID_ong` int NOT NULL,
-  PRIMARY KEY (`ID_doacao`),
-  KEY `ID_doador` (`ID_doador`),
-  KEY `ID_ong` (`ID_ong`)
+CREATE TABLE `categoria` (
+                             `cod` int(11) NOT NULL,
+                             `nome` varchar(80) NOT NULL,
+                             `descricao` varchar(300) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categoria`
+--
+
+INSERT INTO `categoria` (`cod`, `nome`, `descricao`) VALUES
+                                                         (1, '', 'Camiseta'),
+                                                         (2, '', 'Blusa'),
+                                                         (3, '', 'Camisa'),
+                                                         (4, '', 'Calça'),
+                                                         (5, '', 'Short'),
+                                                         (6, '', 'Saia'),
+                                                         (7, '', 'Vestido'),
+                                                         (8, '', 'Jaqueta'),
+                                                         (9, '', 'Suéter'),
+                                                         (10, '', 'Moletom'),
+                                                         (11, '', 'Bermuda'),
+                                                         (12, '', 'Pijama'),
+                                                         (13, '', 'Meias'),
+                                                         (14, '', 'Tênis'),
+                                                         (15, '', 'Sandália'),
+                                                         (16, '', 'Bota');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `doador`
+-- Table structure for table `doacao`
 --
 
-DROP TABLE IF EXISTS `doador`;
-CREATE TABLE IF NOT EXISTS `doador` (
-  `ID_doador` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(80) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `CPF` varchar(30) NOT NULL,
-  `senha` varchar(100) NOT NULL,
-  PRIMARY KEY (`ID_doador`)
+CREATE TABLE `doacao` (
+                          `ID_doacao` int(11) NOT NULL,
+                          `dataDoacao` date DEFAULT NULL,
+                          `ID_doador` int(11) NOT NULL,
+                          `ID_ong` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `doacao`
+--
+
+INSERT INTO `doacao` (`ID_doacao`, `dataDoacao`, `ID_doador`, `ID_ong`) VALUES
+                                                                            (1, '2025-11-12', 1, 1),
+                                                                            (2, '2024-12-02', 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `itemdoacao`
+-- Table structure for table `doador`
 --
 
-DROP TABLE IF EXISTS `itemdoacao`;
-CREATE TABLE IF NOT EXISTS `itemdoacao` (
-  `ID_item` int NOT NULL AUTO_INCREMENT,
-  `qtd` int NOT NULL,
-  `ID_doacao` int NOT NULL,
-  `cod` int NOT NULL,
-  `ID_tamanho` int DEFAULT NULL,
-  PRIMARY KEY (`ID_item`),
-  KEY `ID_doacao` (`ID_doacao`),
-  KEY `cod` (`cod`),
-  KEY `ID_tamanho` (`ID_tamanho`)
+CREATE TABLE `doador` (
+                          `ID_doador` int(11) NOT NULL,
+                          `nome` varchar(80) NOT NULL,
+                          `email` varchar(100) NOT NULL,
+                          `CPF` varchar(30) NOT NULL,
+                          `senha` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `doador`
+--
+
+INSERT INTO `doador` (`ID_doador`, `nome`, `email`, `CPF`, `senha`) VALUES
+    (1, 'Gustavo', 'gu@gu', '123.456.789-88', '123');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `ong`
+-- Table structure for table `itemdoacao`
 --
 
-DROP TABLE IF EXISTS `ong`;
-CREATE TABLE IF NOT EXISTS `ong` (
-  `ID_ong` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(80) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `CNPJ` varchar(30) NOT NULL,
-  `endereco` varchar(100) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `senha` varchar(100) NOT NULL,
-  PRIMARY KEY (`ID_ong`)
+CREATE TABLE `itemdoacao` (
+                              `ID_item` int(11) NOT NULL,
+                              `qtd` int(11) NOT NULL,
+                              `ID_doacao` int(11) NOT NULL,
+                              `cod` int(11) NOT NULL,
+                              `ID_tamanho` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `itemdoacao`
+--
+
+INSERT INTO `itemdoacao` (`ID_item`, `qtd`, `ID_doacao`, `cod`, `ID_tamanho`) VALUES
+    (1, 12, 1, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tamanho`
+-- Table structure for table `ong`
 --
 
-DROP TABLE IF EXISTS `tamanho`;
-CREATE TABLE IF NOT EXISTS `tamanho` (
-  `ID_tamanho` int NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(90) NOT NULL,
-  PRIMARY KEY (`ID_tamanho`)
+CREATE TABLE `ong` (
+                       `ID_ong` int(11) NOT NULL,
+                       `nome` varchar(80) NOT NULL,
+                       `email` varchar(100) NOT NULL,
+                       `CNPJ` varchar(30) NOT NULL,
+                       `endereco` varchar(100) NOT NULL,
+                       `telefone` varchar(20) NOT NULL,
+                       `senha` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
 
 --
--- Estrutura para tabela `telefone`
+-- Dumping data for table `ong`
 --
-
-DROP TABLE IF EXISTS `telefone`;
-CREATE TABLE IF NOT EXISTS `telefone` (
-  `telefone` varchar(30) NOT NULL,
-  `ID_doador` int NOT NULL,
-  KEY `ID_doador` (`ID_doador`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-DROP TABLE IF EXISTS `administrador`;
-CREATE TABLE IF NOT EXISTS `administrador` (
-  `email` varchar(100) NOT NULL,
-  `senha` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Inserir categoria de camiseta
-INSERT INTO categoria (descricao)
-VALUES ('Camiseta');
-
--- Inserir categoria de blusa
-INSERT INTO categoria (descricao)
-VALUES ('Blusa');
-
--- Inserir categoria de camisa
-INSERT INTO categoria (descricao)
-VALUES ('Camisa');
-
--- Inserir categoria de calça
-INSERT INTO categoria (descricao)
-VALUES ('Calça');
-
--- Inserir categoria de short
-INSERT INTO categoria (descricao)
-VALUES ('Short');
-
--- Inserir categoria de saia
-INSERT INTO categoria (descricao)
-VALUES ('Saia');
-
--- Inserir categoria de vestido
-INSERT INTO categoria (descricao)
-VALUES ('Vestido');
-
--- Inserir categoria de jaqueta
-INSERT INTO categoria (descricao)
-VALUES ('Jaqueta');
-
--- Inserir categoria de suéter
-INSERT INTO categoria (descricao)
-VALUES ('Suéter');
-
--- Inserir categoria de moletom
-INSERT INTO categoria (descricao)
-VALUES ('Moletom');
-
--- Inserir categoria de bermuda
-INSERT INTO categoria (descricao)
-VALUES ('Bermuda');
-
--- Inserir categoria de pijama
-INSERT INTO categoria (descricao)
-VALUES ('Pijama');
-
--- Inserir categoria de meias
-INSERT INTO categoria (descricao)
-VALUES ('Meias');
-
--- Inserir categoria de tênis
-INSERT INTO categoria (descricao)
-VALUES ('Tênis');
-
--- Inserir categoria de sandália
-INSERT INTO categoria (descricao)
-VALUES ('Sandália');
-
--- Inserir categoria de bota
-INSERT INTO categoria (descricao)
-VALUES ('Bota');
-
-INSERT INTO tamanho (descricao) VALUES ('PP');
-INSERT INTO tamanho (descricao) VALUES ('P');
-INSERT INTO tamanho (descricao) VALUES ('M');
-INSERT INTO tamanho (descricao) VALUES ('G');
-INSERT INTO tamanho (descricao) VALUES ('GG');
-INSERT INTO tamanho (descricao) VALUES ('XG');
-INSERT INTO tamanho (descricao) VALUES ('XXG');
 
 INSERT INTO `ong` (`ID_ong`, `nome`, `email`, `CNPJ`, `endereco`, `telefone`, `senha`) VALUES
                                                                                            (1, 'Instituto da Esperança', 'contato@institutodaesperanca.com.br', '12.345.678/0001-90', 'Rua da Paz, 123, Centro', '(11) 98765-4321', 'senhaSegura1'),
@@ -236,5 +178,136 @@ INSERT INTO `ong` (`ID_ong`, `nome`, `email`, `CNPJ`, `endereco`, `telefone`, `s
                                                                                            (17, 'Instituto de Direitos Humanos', 'direitos@instituto.org', '77.888.999/0001-25', 'Praça dos Direitos, 987, Centro de Justiça', '(11) 82345-0987', 'senhaSegura17'),
                                                                                            (18, 'Movimento Ecológico', 'ecologico@movimento.org', '99.000.111/0001-26', 'Rua Verde, 654, Vila Ecológica', '(11) 81234-9876', 'senhaSegura18'),
                                                                                            (19, 'Fundação Arte e Cultura', 'arte@fundacao.org', '12.345.678/0001-27', 'Avenida da Arte, 321, Centro Cultural', '(11) 80123-8765', 'senhaSegura19'),
-                                                                                           (20, 'Centro de Inclusão Digital', 'inclusao@centro.org', '98.765.432/0001-28', 'Rua Digital, 987, Vila da Tecnologia', '(11) 79012-7654', 'senhaSegura20');
+                                                                                           (20, 'Centro de Inclusão Digital', 'inclusao@centro.org', '98.765.432/0001-28', 'Rua Digital, 987, Vila da Tecnologia', '(11) 79012-7654', 'senhaSegura20'),
+                                                                                           (21, 'doeRoupas', 'ong@ong', '24.143.432/4343-24', 'Rua da costa', '(12) 31231-2321', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tamanho`
+--
+
+CREATE TABLE `tamanho` (
+                           `ID_tamanho` int(11) NOT NULL,
+                           `descricao` varchar(90) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tamanho`
+--
+
+INSERT INTO `tamanho` (`ID_tamanho`, `descricao`) VALUES
+                                                      (1, 'PP'),
+                                                      (2, 'P'),
+                                                      (3, 'M'),
+                                                      (4, 'G'),
+                                                      (5, 'GG'),
+                                                      (6, 'XG'),
+                                                      (7, 'XXG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telefone`
+--
+
+CREATE TABLE `telefone` (
+                            `telefone` varchar(30) NOT NULL,
+                            `ID_doador` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categoria`
+--
+ALTER TABLE `categoria`
+    ADD PRIMARY KEY (`cod`);
+
+--
+-- Indexes for table `doacao`
+--
+ALTER TABLE `doacao`
+    ADD PRIMARY KEY (`ID_doacao`),
+  ADD KEY `ID_doador` (`ID_doador`),
+  ADD KEY `ID_ong` (`ID_ong`);
+
+--
+-- Indexes for table `doador`
+--
+ALTER TABLE `doador`
+    ADD PRIMARY KEY (`ID_doador`);
+
+--
+-- Indexes for table `itemdoacao`
+--
+ALTER TABLE `itemdoacao`
+    ADD PRIMARY KEY (`ID_item`),
+  ADD KEY `ID_doacao` (`ID_doacao`),
+  ADD KEY `cod` (`cod`),
+  ADD KEY `ID_tamanho` (`ID_tamanho`);
+
+--
+-- Indexes for table `ong`
+--
+ALTER TABLE `ong`
+    ADD PRIMARY KEY (`ID_ong`);
+
+--
+-- Indexes for table `tamanho`
+--
+ALTER TABLE `tamanho`
+    ADD PRIMARY KEY (`ID_tamanho`);
+
+--
+-- Indexes for table `telefone`
+--
+ALTER TABLE `telefone`
+    ADD KEY `ID_doador` (`ID_doador`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categoria`
+--
+ALTER TABLE `categoria`
+    MODIFY `cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `doacao`
+--
+ALTER TABLE `doacao`
+    MODIFY `ID_doacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `doador`
+--
+ALTER TABLE `doador`
+    MODIFY `ID_doador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `itemdoacao`
+--
+ALTER TABLE `itemdoacao`
+    MODIFY `ID_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ong`
+--
+ALTER TABLE `ong`
+    MODIFY `ID_ong` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tamanho`
+--
+ALTER TABLE `tamanho`
+    MODIFY `ID_tamanho` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
